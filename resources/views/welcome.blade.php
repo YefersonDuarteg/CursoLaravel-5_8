94d9ae071e3e293a3759aecf159f9736d315aa72
YefersonDuarteg <yimmy0831@gmail.com>
2022-06-26 00:28:02 -0500

    cambio al titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravelaaaaaaaaaaaaaa
+                    Laravel -- Duarteg
                 </div>
 
                 <div class="links">
