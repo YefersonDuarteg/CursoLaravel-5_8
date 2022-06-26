2833cee65fede2fd1a0d2781c77768733d034fe1
YefersonDuarteg <yimmy0831@gmail.com>
2022-06-26 00:11:39 -0500

    Primer comit del proyeto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel - esta es la prueba
+                    Laravel
                 </div>
 
                 <div class="links">
